932b7a3e9e63e2156a4d48a82307cdf1b8122064
Stalin Amagua <stalin.amagua@outlook.com>
2026-05-05 23:22:06 -0500

    4-primer-proyecto0en-angular completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-c104');
+  protected readonly title = signal('jueltter');
 }
