cefd3272f745318bcd299b75da7500057cd7880c
Stalin Amagua <stalin.amagua@outlook.com>
2026-05-06 00:54:04 -0500

    4-tarea-reforzar-lo-aprendido-hasta-el-momento completed

---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  imports: [UpperCasePipe],
+})
+export class HeroPageComponent {
+  name = signal('Ironman');
+  age = signal(45);
+
+  getHeroDescription() {
+    return `${this.name()} - ${this.age()}`;
+  }
+
+  changeHero() {
+    this.name.set('Spiderman');
+    this.age.set(22);
+  }
+
+  changeAge() {
+    this.age.set(60);
+  }
+
+  resetForm() {
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+}
