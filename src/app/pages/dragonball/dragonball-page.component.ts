dd60a3ef2146d19ddfd1b487d64b1514ab2c699f
Stalin Amagua <samagua@finanzas.gob.ec>
2026-05-07 16:16:16 -0500

    5-tarea-preperacion-pagina completed

---
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-dragonball-page',
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css'
+})
+export class DragonballPageComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
