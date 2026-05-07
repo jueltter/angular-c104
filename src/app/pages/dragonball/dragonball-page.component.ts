5f4303e4f709381644d97a4c21176162c4353f01
Stalin Amagua <samagua@finanzas.gob.ec>
2026-05-07 16:46:09 -0500

    5-@if-mostrar-elementos-de-forma-condicional completed

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -19,7 +19,8 @@ export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 10000 },
     { id: 2, name: 'Vegeta', power: 8500 },
-    { id: 3, name: 'Gohan', power: 7000 }
+    { id: 3, name: 'Gohan', power: 7000 },
+    { id: 4, name: 'Yamcha', power: 500 }
   ]);
 
   powerClasses = computed(() => {
