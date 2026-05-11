c77ee2ee87059fd9a73db97ef75bb576a3f8bded
Stalin Amagua <samagua@finanzas.gob.ec>
2026-05-11 10:59:02 -0500

    5-inpunts-agregar-personaje completed

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -16,6 +16,10 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal("Gohan");
+  power = signal(100);
+
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 10000 },
     { id: 2, name: 'Vegeta', power: 8500 },
@@ -31,4 +35,10 @@ export class DragonballPageComponent {
   });
 
 	constructor() {}
+
+  addCharacter() {
+    this.characters.update(chars => [...chars, { id: chars.length + 1, name: this.name(), power: this.power() }]);
+    this.name.set('');
+    this.power.set(0);
+  }
 }
